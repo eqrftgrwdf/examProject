ee7de15cd99daea4b0e69f17b4a19a4c884a6b46
eqrftgrwdf <dimocnhikhouse@gmail.com>
2025-07-01 19:38:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,8 +51,9 @@ void addBlockToEnd(int*& arr, int& size, int* block, int blockSize) {
 
 void printArray(int* arr, int size) {
 	cout << "Array: ";
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++) {
 		cout << arr[i] << " ";
+	}
 	cout << endl;
 }
 int main() {
